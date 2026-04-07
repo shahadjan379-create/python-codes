438f3f1c8c92b68bcaed793451bb1688255a74e4
shahadjan379 <shahadjan379@gmail.com>
2026-04-06 22:04:23 -0700

    3 commit

---
+++ code3.py
@@ -0,0 +1,9 @@
+color= str(input("enter the traffic light color: "))
+if color=="red":
+    print("You must stop.")
+elif color=="yellow":
+    print("Be ready to move.")
+elif color=="green":
+    print("You can move.")
+else:
+    print("traffic signal isn't working right.")
