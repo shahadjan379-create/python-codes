5e55b3001dc0dfa315fe61758cf5edff95eac29b
shahadjan379 <shahadjan379@gmail.com>
2026-04-06 22:11:54 -0700

    4 commit

---
+++ code4.py
@@ -0,0 +1,5 @@
+fuel= float(input("Enter how much petrol is left in your car (in liter): "))
+if fuel<0.25:
+    print("Please refill the fuel in your car")
+else:
+    print("you still have enough fuel")
