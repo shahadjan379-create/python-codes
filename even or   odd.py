93bf41fabd6110b1fc24ce689f0e907518bd14d1
shahadjan379 <shahadjan379@gmail.com>
2026-04-07 21:05:24 -0700

    8 commit

---
+++ even or   odd.py
@@ -0,0 +1,7 @@
+value = int(input("enter a positive integer: "))
+if value % 2 ==0:
+    print ("The vlaue is even.")
+elif value % 2 == 1:
+    print ("The value is odd.")
+else:
+    print ("the number is negative.")
